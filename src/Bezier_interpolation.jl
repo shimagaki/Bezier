2a781158947a1f70e9166b781c6c0b241fc1f2e9
kai <kai0shimagaki@gmail.com>
2021-10-10 09:27:27 -0700

    Include 2nd order Bezier

--- src/Bezier_interpolation.jl
+++ src/Bezier_interpolation.jl
@@ -1,5 +1,7 @@
 Bez(t, p1, p2, a, b) = (1-t)^3 * p1 + 3 * t * (1-t)^2 * a + 3 * t^2 * (1-t) * b + t^3 * p2; 
+Bez_2ndOrder(t, p1, p2, a) = (1-t)^2 * p1 + 2*t*(1-t)*a + t^2 * p2;
 
+# 3rd order Bezier
 function get_a2b_Bez(x,a)
     N = length(a)
     b = zeros(N)
@@ -10,6 +12,15 @@ function get_a2b_Bez(x,a)
     return b
 end;
 
+function get_a_Bezier_2ndOrder(p_set)
+    N = size(p_set,1)-1
+    avec = zeros(N)
+    avec[1] = p_set[1]
+    for n in 2:N
+        avec[n] = 2*p_set[n]-avec[n-1]
+    end
+    return avec
+end;
 
 function get_BezMat(N)
     Bez_mat = zeros(N,N);
@@ -22,6 +33,7 @@ function get_BezMat(N)
     Bez_mat[N,N-1] = 2; Bez_mat[N,N] = 7;
     return Bez_mat
 end;
+
 function get_BezVec(N, x)
     Bez_vec = zeros(N);
     for n in 2:(N-1)
@@ -43,6 +55,8 @@ function get_a_b_Bezier(N, p_set)
     b_vec = get_a2b_Bez(p_set, a_vec);
     return (a_vec, b_vec)
 end; 
+
+
 function get_integrated_Bezier_2nd()
     a = 1.0/7
     b = 1.0/42
@@ -51,6 +65,12 @@ function get_integrated_Bezier_2nd()
     return B2 = Matrix([[a, b, c, d] [b, c, d, c] [c, d, c, b] [d, c, b, a]])
 end;
 
+function get_integrated_Bezier_2nd_2ndOrder()
+    a = 1.0/5
+    b = 1.0/20
+    c = 1.0/30
+    return B2 = Matrix([[a, b, c] [b, c, b] [c, b, a]])
+end;
 
 function get_Bezier_vec_Mat_for_all_sites_time(L, x1_traject, x2_traject)
     #--Interpolation points for single site freq. for each time --#
@@ -86,3 +106,50 @@ function get_Bezier_vec_Mat_for_all_sites_time(L, x1_traject, x2_traject)
     end
     return (point_set_x1_a, point_set_x1_b, point_set_x2_a, point_set_x2_b)
 end;
+
+function get_Bezier_vec_Mat_for_all_sites_time_2ndOrder(L, x1_traject, x2_traject)
+    #--Interpolation points for single site freq. for each time --#
+    n_traject = size(x1_traject,1)
+    N = n_traject-1
+    point_set_x1 = zeros(L, n_traject)
+    point_set_x1_a = zeros(L, N)
+    for i in 1:L
+        point_set_x1[i,:] = [ x1_traject[n][i] for n in 1:n_traject];
+        size(point_set_x1), size(point_set_x1[i,:]), N
+        (a_vec_i) = get_a_Bezier_2ndOrder(point_set_x1[i,:])
+        #@show size(a_vec_i), size(b_vec_i)
+        point_set_x1_a[i,:] = copy(a_vec_i)
+    end
+    #--------------------------#
+    #-- Interpolation points for corr. for each time --#
+    point_set_x2 = zeros(L,L, n_traject)
+    point_set_x2_a = zeros(L,L, N)
+
+    for i in 1:L
+        for j in i:L
+            point_set_x2[i,j,:] = [ x2_traject[n][i,j] for n in 1:n_traject];
+            (a_vec_i) = get_a_Bezier_2ndOrder(point_set_x2[i,j,:])
+            point_set_x2_a[i,j,:] = copy(a_vec_i)
+            point_set_x2_a[j,i,:] = copy(a_vec_i)
+
+        end
+    end
+    return (point_set_x1_a, point_set_x2_a)
+end;
+
+
+
+"""
+Retuns the interpolation: n_out is the number of points that can interpolate between the observation points [p_n, p_{n+1}]. 
+"""
+function get_bezier_interpolation_periodic(n_out, x_set, p_set_left, p_set_right, a_vec, b_vec)
+    scale_n_out = 1.0 / (n_out-1)
+    #n_interpolation = 10; scale_interpolation = 1.0/n_interpolation
+    x_axis = collect(x_set[1]:(x_set[2]-1))
+    y_axis = Bez.( scale_n_out*collect(0:(n_out-1)), p_set_left[1], p_set_right[1], a_vec[1], b_vec[1]);
+    for n in 1:(N-1)
+        x_axis = vcat(x_axis, collect(x_set[n+1]:(x_set[n+2]-1)) )
+        y_axis = vcat(y_axis, Bez.( scale_n_out*collect(0:(n_out-1)), p_set_left[n+1], p_set_right[n+1], a_vec[n+1], b_vec[n+1]))
+    end
+    return (x_axis, y_axis)
+end
